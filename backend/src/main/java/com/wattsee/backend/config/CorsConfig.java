3b7472f2ddcc40f148cc497ba4c692aeb66681ca
João Medeiros <joaomanoelmedeirosmatos@gmail.com>
2026-08-18 14:10:46 -0300

    Ajuste CORS

--- backend/src/main/java/com/wattsee/backend/config/CorsConfig.java
+++ backend/src/main/java/com/wattsee/backend/config/CorsConfig.java
@@ -5,11 +5,16 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig  implements WebMvcConfigurer {
+public class CorsConfig implements WebMvcConfigurer {
+
     @Override
-    public void addCorsMappings(CorsRegistry registry){
-        registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:3000", "https://wattsee-jm15pz35y-medeiros20s-projects.vercel.app/")
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns(
+                        "http://localhost:3000",
+                        "https://*.vercel.app",
+                        "https://wattsee-api.duckdns.org"
+                )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
                 .allowedHeaders("*")
                 .allowCredentials(false)
